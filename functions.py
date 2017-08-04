40c455bad081fbecdb47a867ae5b95cb1fb42623
iigp2k18 <iigp2k18@gmail.com>
2017-08-04 17:06:48 +0530

    fixed namespace for rosservice call

--- functions.py
+++ functions.py
@@ -79,9 +79,9 @@ def setpoint_local_position(lx, ly, lz, yaw, tolerance = 2.0, async = False, rel
         rospy.logerr("pos set service call failed %s", e)
 
 def setpoint_global_position(lat_x, long_y, rel_alt_z, yaw, tolerance= 0.0, async = False, yaw_valid= False):
-    rospy.wait_for_service('/<namespace>/navigation/position_set_global')
+    rospy.wait_for_service('/flytos/navigation/position_set_global')
     try:
-        handle = rospy.ServiceProxy('/<namespace>/navigation/position_set_global', PositionSetGlobal)
+        handle = rospy.ServiceProxy('/flytos/navigation/position_set_global', PositionSetGlobal)
 
         # build message structure
         req_msg = PositionSetGlobalRequest(lat_x=lat_x, long_y=long_y, rel_alt_z=rel_alt_z, yaw=yaw, tolerance=tolerance, async=async, yaw_valid=yaw_valid)
@@ -92,9 +92,9 @@ def setpoint_global_position(lat_x, long_y, rel_alt_z, yaw, tolerance= 0.0, asyn
         rospy.logerr("global pos set service call failed %s", e)
 
 def setpoint_velocity(vx, vy, vz, yaw_rate, tolerance= 1.0, async = False, relative= False, yaw_rate_valid= False, body_frame= False):
-    rospy.wait_for_service('/<namespace>/navigation/velocity_set')
+    rospy.wait_for_service('/flytos/navigation/velocity_set')
     try:
-        handle = rospy.ServiceProxy('/<namespace>/navigation/velocity_set', VelocitySet)
+        handle = rospy.ServiceProxy('/flytos/navigation/velocity_set', VelocitySet)
         # build message structure
         req_msg = VelocitySetRequest(vx=vx, vy=vy, vz=vz, yaw_rate=yaw_rate, tolerance=tolerance, async=async, relative=relative, yaw_rate_valid=yaw_rate_valid, body_frame=body_frame)
         resp = handle(req_msg)
