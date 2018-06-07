e1e8cfabe7be866678ec9ad4e0c0dd012b73e750
shivcapoor <shivcapoor95@gmail.com>
2018-06-07 15:46:03 +0530

    added separate file for functions and added namespace, arm and access request functions

--- drone_main.py
+++ drone_main.py
@@ -1,23 +1,6 @@
 import rospy
 from core_api.srv import *
+from functions import *
 
-def get_global_namespace():
-    rospy.wait_for_service('/get_global_namespace')
-    try:
-        res = rospy.ServiceProxy('/get_global_namespace', ParamGetGlobalNamespace)
-        op = res()
-        return str(op.param_info.param_value)
-    except rospy.ServiceException, e:
-        rospy.logerr("global namespace service not available", e)
-        return None
-
-def access_request(enable_access):
-    rospy.wait_for_service('/<namespace>/navigation/access_request')
-    try:
-        handle = rospy.ServiceProxy('/<namespace>/navigation/access_request', AccessRequest)
-        resp = handle(enable_access=enable_access)
-        return resp
-    except rospy.ServiceException, e:
-        rospy.logerr("service call failed %s", e)
 
 print(get_global_namespace())
---
+++ functions.py
@@ -0,0 +1,27 @@
+def get_global_namespace():
+    rospy.wait_for_service('/get_global_namespace')
+    try:
+        res = rospy.ServiceProxy('/get_global_namespace', ParamGetGlobalNamespace)
+        op = res()
+        return str(op.param_info.param_value)
+    except rospy.ServiceException, e:
+        rospy.logerr("global namespace service not available", e)
+        return None
+
+def access_request(enable_access):
+    rospy.wait_for_service('/flytos/navigation/access_request')
+    try:
+        handle = rospy.ServiceProxy('/flytos/navigation/access_request', AccessRequest)
+        resp = handle(enable_access=enable_access)
+        return resp
+    except rospy.ServiceException, e:
+        rospy.logerr("service call failed %s", e)
+
+def arm()
+    rospy.wait_for_service('/<namespace>/navigation/arm')
+    try:
+        handle = rospy.ServiceProxy('/<namespace>/navigation/arm', Arm)
+        resp = handle()
+        return resp
+    except rospy.ServiceException, e:
+        rospy.logerr("service call failed %s", e)
