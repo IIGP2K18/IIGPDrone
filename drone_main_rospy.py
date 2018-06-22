ec00ec332f7c8cf0467717478df8ea40ac382d4c
shivcapoor <shivcapoor95@gmail.com>
2018-06-22 16:26:39 +0530

    Added a small mission to drone main code

--- drone_main_rospy.py
+++ drone_main_rospy.py
@@ -1,6 +1,40 @@
-from functions import *
+import time
+#from functions import *
 
-print(access_request(True))
-print("Namespace is: "+get_global_namespace())
+tkoffalt = 3.0
+leglen = 3.0
+stdelay = 5
 
-arm()
+print "Please arm drone from the RC"
+
+for i in range(stdelay):
+	print "Beginning in ",stdelay-i," seconds"
+	time.sleep(1)
+
+print "Starting the mission"
+
+print "Taking off"
+takeoff(tkoffalt)
+time.sleep(1)
+
+print "On Leg 1"
+setpoint_local_position(leglen,leglen,0,0,tolerance=0.5,relative=True)
+time.sleep(1)
+
+print "On Leg 2"
+setpoint_local_position(leglen,-leglen,0,0,tolerance=0.5,relative=True)
+time.sleep(1)
+
+print "On Leg 3"
+setpoint_local_position(-leglen,-leglen,0,0,tolerance=0.5,relative=True)
+time.sleep(1)
+
+print "On Leg 4"
+setpoint_local_position(-leglen,leglen,0,0,tolerance=0.5,relative=True)
+time.sleep(1)
+
+print "Landing"
+land()
+
+print "Disarming"
+disarm()
