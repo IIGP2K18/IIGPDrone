61bbe3603cfd1d3c7d2a6e09f19e50e9fd352478
shivcapoor <shivcapoor95@gmail.com>
2018-06-22 16:48:08 +0530

    Made mission as a function

--- drone_main_rospy.py
+++ drone_main_rospy.py
@@ -1,40 +1,47 @@
 import time
-#from functions import *
+from functions import *
 
 tkoffalt = 3.0
-leglen = 3.0
-stdelay = 5
+leglen = 3.0 #length of each leg in the mission
+stdelay = 5 #starting delay for mission in seconds
+wprest  = 2 #rest interval after reaching each waypoint in seconds
 
-print "Please arm drone from the RC"
+def mission_1():
+	print "***Please arm drone from the RC***"
+	time.sleep(2)
 
-for i in range(stdelay):
-	print "Beginning in ",stdelay-i," seconds"
-	time.sleep(1)
+	for i in range(stdelay):
+		print "Beginning in ",stdelay-i," seconds"
+		time.sleep(1)
 
-print "Starting the mission"
+	print "***Starting the mission***"
 
-print "Taking off"
-takeoff(tkoffalt)
-time.sleep(1)
+	print "Taking off"
+	takeoff(tkoffalt)
+	time.sleep(wprest)
 
-print "On Leg 1"
-setpoint_local_position(leglen,leglen,0,0,tolerance=0.5,relative=True)
-time.sleep(1)
+	print "On Leg 1"
+	setpoint_local_position(leglen,leglen,0,0,tolerance=0.5,relative=True)
+	time.sleep(wprest)
 
-print "On Leg 2"
-setpoint_local_position(leglen,-leglen,0,0,tolerance=0.5,relative=True)
-time.sleep(1)
+	print "On Leg 2"
+	setpoint_local_position(leglen,-leglen,0,0,tolerance=0.5,relative=True)
+	time.sleep(wprest)
 
-print "On Leg 3"
-setpoint_local_position(-leglen,-leglen,0,0,tolerance=0.5,relative=True)
-time.sleep(1)
+	print "On Leg 3"
+	setpoint_local_position(-leglen,-leglen,0,0,tolerance=0.5,relative=True)
+	time.sleep(wprest)
 
-print "On Leg 4"
-setpoint_local_position(-leglen,leglen,0,0,tolerance=0.5,relative=True)
-time.sleep(1)
+	print "On Leg 4"
+	setpoint_local_position(-leglen,leglen,0,0,tolerance=0.5,relative=True)
+	time.sleep(wprest)
 
-print "Landing"
-land()
+	print "Landing"
+	land()
 
-print "Disarming"
-disarm()
+	print "Disarming"
+	disarm()
+
+	print "***Mission complete***"
+
+mission_1()
